28dc34e4ca59263fc44cad977712bdcd3a65b933
dopezinho <opugnero@gmail.com>
2023-03-16 14:07:37 -0300

    home columns error

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
+import './index.scss'
 
 const MovieCard = ({ movieProp }) => {
   return (
-    <div className="MovieCard">
-      <div className="movie-image">
+    <div className="movie">
+      <div className="movie__image">
         <img src={`https://image.tmdb.org/t/p/w1280${movieProp.poster_path}`} alt={movieProp.title} />
       </div>
-      <div className="movie-description">
+      <div className="movie__description">
         <h3>{movieProp.title}</h3>
-        <Link to={`/movie/${movieProp.id}`} className="btn-details">
+        <Link to={`/movie/${movieProp.id}`} className="movie__btn-details">
           Ver Detalhes
         </Link>
       </div>
